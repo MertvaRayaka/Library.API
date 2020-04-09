05b7d78f62c1f190f83ebb2604f965d8a52773f4
631203498@qq.com <631203498@qq.com>
2020-04-09 23:36:31 +0800

    向数据库增加、删除数据测试

--- Library.API/Entities/LibraryDbContext.cs
+++ Library.API/Entities/LibraryDbContext.cs
@@ -6,14 +6,35 @@ using System.Threading.Tasks;
 
 namespace Library.API.Entities
 {
-    public class LibraryDbContext:DbContext
+    public class LibraryDbContext : DbContext
     {
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
 
-        public LibraryDbContext(DbContextOptions<LibraryDbContext> options):base(options)
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.SeedData();
+        }
+    }
+
+    public static class ModelBuilderExtension
+    {
+        public static void SeedData(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasData(new Author
+            {
+                Id = Guid.NewGuid(),
+                Name = "Neil",
+                BirthDate = new DateTimeOffset(new DateTime(1993, 12, 19)),
+                BirthPlace = "江苏·如东",
+                Email = "author@xxx.com"
+            }); ;
         }
     }
 }
---
+++ Library.API/Migrations/20200409152704_SeedData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Library.API.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "BirthDate", "BirthPlace", "Email", "Name" },
+                values: new object[] { new Guid("8a970d4f-1584-4774-8969-ac8783bc6fd0"), new DateTimeOffset(new DateTime(1993, 12, 19, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 8, 0, 0, 0)), "江苏·如东", "author@xxx.com", "Neil" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: new Guid("8a970d4f-1584-4774-8969-ac8783bc6fd0"));
+        }
+    }
+}
